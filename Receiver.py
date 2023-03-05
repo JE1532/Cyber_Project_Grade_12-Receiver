76f49eed249f965b9fbad612e03f7e904b6d8810
JE1532 <jonatanalf2016@gmail.com>
2023-03-05 15:59:33 +0200

    Initial commit

---
+++ Receiver.py
@@ -0,0 +1,21 @@
+class Receiver:
+    def __init__(self ,input_socks, output_queue):
+        self.input_socks = input_socks
+        self.output_queue = output_queue
+        self.start()
+
+
+    def start(self):
+        while True:
+            for sock in self.input_socks:
+                input_ = sock.recv()
+                if input_:
+                    self.output_queue.put((sock, input_))
+
+
+    def return_socket(self, sock):
+        self.input_socks.add(sock)
+
+
+    def remove(self, sock):
+        self.input_socks.remove(sock)
